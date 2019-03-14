3b20fbbf7b1aaf69d0abc2a028af1efcdf0dbada
johnnyzhang123 <33502662+johnnyzhang123@users.noreply.github.com>
2019-03-13 17:47:03 -0700

    Update bitcoin_hash.sv

--- bitcoin_hash.sv
+++ bitcoin_hash.sv
@@ -1,4 +1,4 @@
-//1178	1389	2567	201.94	2200	10.894	27.966
+1259	1133	2392	189.72	2200	11.596	27.738
 module bitcoin_hash (input logic clk, reset_n, start,
  input logic [15:0] message_addr, output_addr,
  output logic done, mem_clk, mem_we,
